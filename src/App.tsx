b6ed6f25f93e9eef11c4337027353b17cec312df
jpm1996 <jaopmo1996@gmail.com>
2021-06-30 07:14:32 -0300

    segunda aula concluida

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import { AuthContextProvider } from "./contexts/AuthContext";
+type User = {
+  id: string;
+  name: string;
+  avatar: string;
+};
+
+type AuthContextType = {
+  user: User | undefined;
+  signInWithGoogle: () => Promise<void>;
+};
+
+function App() {
+  
+
+  return (
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
